c206528362c04036390476a9c0f515041d73c5c5
EricHaominLi <haomin.li@verizonmedia.com>
2020-02-23 05:22:17 -0800

    add marker UI

--- component/MyMap.js
+++ component/MyMap.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
-
+import AddDialog from '../component/AddDialog'
 import SignIn from '../component/SignIn'
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Button } from '@material-ui/core';
 
 
       
@@ -12,49 +13,129 @@ class MyMap extends React.Component{
         latitude:null,
         longitude:null,
         loaded:false,
+        data: [],
+        editEnable: false,
+        curSelect:null,
+        addDialog:false,
+        curSelectPos:null,
     }
   }
-  componentDidMount(){
-  if (navigator.geolocation) {
-      console.log("here");
-      navigator.geolocation.getCurrentPosition((position)=>{
-          console.log("good");
-          console.log(position.coords.latitude);
-          this.setState({
-              latitude:position.coords.latitude,
-              longitude:position.coords.longitude,
-              loaded:true,
-          });
-      });
-    } else {
+
+  diaLogEnd(){
       this.setState({
-          latitude:36.974117,
-          longitude:-122.030792,
-          loaded:true,
-      });
-    }
+          addDialog: false,
+      })
+  }
+
+  postUpdate(){
+    fetch(
+        'https://hackforhumanity.appspot.com/marker/',
+        {method: 'GET'}
+    ).then(
+        res =>res.json()
+    ).then(
+        (data)=>{
+            console.log("data added: ", data);
+            let toReturn = [];
+            data.forEach(
+                (pos)=>{
+                    const posi={
+                        lat: pos.lat,
+                        lng: pos.long,
+                    }
+                    toReturn.push(
+                        <Marker position={posi}> </Marker>
+                    );
+                }
+            );
+            this.setState({
+                data: toReturn,
+                addDialog: false,
+            });
+        }
+    );
     
+  }
+
+  componentDidMount(){
+    fetch(
+        'https://hackforhumanity.appspot.com/marker/',
+        {method: 'GET'}
+    ).then(
+        res =>res.json()
+    ).then(
+        (data)=>{
+            let toReturn = [];
+            data.forEach(
+                (pos)=>{
+                    const posi={
+                        lat: pos.lat,
+                        lng: pos.long,
+                    }
+                    toReturn.push(
+                        <Marker position={posi}> </Marker>
+                    );
+                }
+            );
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position)=>{
+                    this.setState({
+                        latitude:position.coords.latitude,
+                        longitude:position.coords.longitude,
+                        loaded:true,
+                        data: toReturn,
+                    });
+                });
+              } else {
+                this.setState({
+                    latitude:36.974117,
+                    longitude:-122.030792,
+                    loaded:true,
+                    data: toReturn,
+                });
+              }
+        }
+    )
 }
 
+    mapClicked(mapProps, map, clickEvent){
+        const curLat = clickEvent.latLng.lat();
+        const curLng = clickEvent.latLng.lng();
+        const curPos={
+            lat:curLat,
+            lng:curLng,
+        }
+        let temp = (
+            <Marker position={curPos}
+            onClick={()=>{
+                this.setState(
+                    {addDialog: true}
+                );
+            }}
+            >
+                </Marker>
+        );
+        this.setState({
+            curSelect:temp,
+            curSelectPos:curPos,
+        })
+    }
 
-
-getData() {
-    const xhr = new XMLHttpRequest();
-    const url = 'https://hackforhumanity.appspot.com/marker/';
-   
-    xhr.open('GET', url);
-    xhr.send();
-  }
 
   render(){
+      const p = this.postUpdate.bind(this);
+      const c = this.diaLogEnd.bind(this);
+      let helper = this.mapClicked.bind(this);
     const mapStyles = {
-      width: '100%',
-      height: '100%',
+      width: '80%',
+      height: '80%',
     };
     if(this.state.loaded===false){
         return <div></div>
     }
-    this.getData();
+    
+     
+    
     return(
     <div className="container">
     <Head>
@@ -63,16 +144,25 @@ getData() {
     </Head>
 
     <main>
-
-
+        {this.state.addDialog?(<AddDialog op={this.state.addDialog} Close={c} pos={this.state.curSelectPos}
+        postUpdate={p}>
+        </AddDialog>):null}
     <Map
         google={this.props.google}
         zoom={12}
         style={mapStyles}
         initialCenter={{ lat: this.state.latitude, lng: this.state.longitude}}
+        onClick={ helper}
       >
+          <Marker position={
+              {lat: 12,
+                lng:15}
+          }>  </Marker>
+          {this.state.data}
+          {this.state.curSelect}
         </Map>
     </main>
+    <Button />
   </div>);
   }
 }
